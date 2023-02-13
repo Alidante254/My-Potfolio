b87cb98a71315a1b2e81a3975f87b5023b384c8d
PETER IRUNGU <peteralidante254@gmail.com>
2023-02-13 23:32:51 +0300

    adding new features

---
+++ script.js
@@ -0,0 +1,9 @@
+    function contactDisplay() {
+        var element = document.getElementById("contact");
+        if (element.style.display === "block") {
+          element.style.display = "none";
+        } else {
+          element.style.display = "block";
+        }
+      }
+      
