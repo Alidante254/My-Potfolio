02290966a9664c7132c6e3da0f430cb23e90a44e
PETER IRUNGU <peteralidante254@gmail.com>
2023-02-15 01:29:14 +0300

    adding a slider

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+    //function to display the send message box
     function contactDisplay() {
         var element = document.getElementById("contact");
         if (element.style.display === "block") {
@@ -6,4 +7,22 @@
           element.style.display = "block";
         }
       }
+
+    //function to set a scrollable image carousel
+      var slideIndex = 1;
+      showSlides(slideIndex);
       
+      function plusSlides(n) {
+        showSlides(slideIndex += n);
+      }
+
+      function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("slides");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+        }
+        slides[slideIndex-1].style.display = "block";
+      }
